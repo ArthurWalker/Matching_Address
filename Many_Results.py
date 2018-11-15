fc07bebfd9228dff4066395dbb9b43dac5951873
ArthurWalker <phuc.phamhuu2@dcu.ie>
2018-11-15 17:43:31 +0000

    Get more info for many results

--- Many_Results.py
+++ Many_Results.py
@@ -9,28 +9,31 @@ def match(row,df):
     return row
 
 def fix(row,dict,geo_df):
-    df_geo_add_ref = geo_df[geo_df['ADDRESS_REFERENCE'].isin(dict[float(row['DwellingData_id'])])]
-    search_city = df_geo_add_ref[df_geo_add_ref.loc[:,'PRINCIPAL_POST_TOWN']==row['MPRN city']|df_geo_add_ref.loc[:,'SECONDARY_LOCALITY']==row['MPRN city']|df_geo_add_ref.loc[:,'LOCALITY']==row['MPRN city']]
-    if (search_city.shape[0]>0):
-        if (search_city.shape[0]==1):
-            if search_city.iloc[0]['ADDRESS_REFERENCE']==row['ADDRESS_REFERENCE']:
-                row['New_Change']=False
-            else:
-                row['New_Change']=True
-                row=match(row,search_city)
-        else:
-            search_thoroughfare = search_city[search_city.loc[:,'THOROUGHFARE']==row['MPRN street'] |search_city.loc[:,'THOROUGHFARE']==row['MPRN MPRN address4']]
-            if (search_thoroughfare.shape[0]>0):
-                if (search_thoroughfare.shape[0]==1):
-                    if search_thoroughfare.iloc[0]['ADDRESS_REFERENCE'] == row['ADDRESS_REFERENCE']:
-                        row['New_Change'] = False
-                    else:
-                        row['New_Change'] = True
-                        row = match(row, search_thoroughfare)
+    if (row['DwellingData_id'] in dict):
+        df_geo_add_ref = geo_df[geo_df['ADDRESS_REFERENCE'].isin(dict[float(row['DwellingData_id'])])]
+        search_city = df_geo_add_ref[df_geo_add_ref.loc[:,'PRINCIPAL_POST_TOWN']==row['MPRN city']]
+        if (search_city.shape[0]>0):
+            if (search_city.shape[0]==1):
+                if search_city.iloc[0]['ADDRESS_REFERENCE']==row['ADDRESS_REFERENCE']:
+                    row['New_Change']=False
                 else:
-                    row['List_results']=list(search_thoroughfare.Full_Address)
+                    row['New_Change']=True
+                    row=match(row,search_city)
             else:
-                row['List_results'] = list(search_city.Full_Address)
+                search_thoroughfare = search_city[search_city.loc[:,'THOROUGHFARE'].str.contains(row['MPRN street']) | search_city.loc[:,'THOROUGHFARE'].str.contains(row['MPRN address4'])]
+                if (search_thoroughfare.shape[0]>0):
+                    if (search_thoroughfare.shape[0]==1):
+                        if search_thoroughfare.iloc[0]['ADDRESS_REFERENCE'] == row['ADDRESS_REFERENCE']:
+                            row['New_Change'] = False
+                        else:
+                            row['New_Change'] = True
+                            row = match(row, search_thoroughfare)
+                    else:
+                        row['List_results']=list(search_thoroughfare.ADDRESS_REFERENCE)
+                else:
+                    row['List_results'] = list(search_city.ADDRESS_REFERENCE)
+        else:
+            row['New_Change'] = False
     else:
         row['New_Change']=False
     return row
@@ -39,6 +42,7 @@ def fix(row,dict,geo_df):
 def dealing_with_MANY_RESULTS(dwelling_df,geo_df,dict):
     dwelling_df['New_Change'] = ""
     dwelling_df['List_results']=""
-    dwelling_df = dwelling_df.apply(fix,args=(dict,geo_df))
+    dwelling_df = dwelling_df.apply(fix,args=(dict,geo_df,),axis=1)
 
     return dwelling_df
+
